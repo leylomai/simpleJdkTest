16051cc80b6fc2b471afae23ccd14d77aea42904
zhanlele <leylomai@foxmail.com>
2019-11-20 17:01:15 +0800

    1120-Callable\Future

---
+++ src/myjava/concurrent/callableFuture/MatchCounter.java
@@ -0,0 +1,70 @@
+package myjava.concurrent.callableFuture;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
+
+public class MatchCounter implements Callable<Integer> {
+    private File directory;
+    private String keyword;
+
+    public MatchCounter(File directory, String keyword) {
+        this.directory = directory;
+        this.keyword = keyword;
+    }
+
+    /**returns the number of matching files*/
+    @Override
+    public Integer call() throws Exception {
+        int count = 0;
+        try {
+            File[] files = directory.listFiles();
+            List<Future<Integer>> results = new ArrayList<>();
+
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    MatchCounter counter = new MatchCounter(file, keyword);
+                    FutureTask<Integer> task = new FutureTask<>(counter);
+                    results.add(task);
+                    Thread t = new Thread(task);
+                    t.start();
+                } else {
+                    if (search(file))
+                        count++;
+                }
+            }
+
+            for (Future<Integer> result : results) {
+                try {
+                    count += result.get();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+        } catch (Exception e) {
+
+        }
+        return count;
+    }
+
+    public boolean search(File file) {
+        try {
+            try (Scanner in = new Scanner(file, "UTF-8")) {
+                boolean found = false;
+                while (!found && in.hasNextLine()) {
+                    String line = in.nextLine();
+                    if (line.contains(keyword))
+                        found = true;
+                }
+                return found;
+            }
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
--- src/myjava/concurrent/concurrentMapEntry/ConcurrentMappingEntry.java
+++ src/myjava/concurrent/concurrentMapEntry/ConcurrentMappingEntry.java
@@ -5,15 +5,15 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class ConcurrentMappingEntry {
     public static void main(String[] args) {
-        /*Map<String, String> map = new ConcurrentHashMap<>();
-        map.put("1", "2");
+        Map<String, String> map = new ConcurrentHashMap<>();
+        //map.put("1", "2");
         String oldValue = map.putIfAbsent("1", "1");
 
-        System.out.println(oldValue);*/
+        System.out.println(oldValue + "###" + map.get("1"));
 
-        StatStudentEntity statStudentEntity = new StatStudentEntity();
+        /*StatStudentEntity statStudentEntity = new StatStudentEntity();
         statStudentEntity.normalMethod();
-        statStudentEntity.mergeMethod();
+        statStudentEntity.mergeMethod();*/
 
     }
 }
