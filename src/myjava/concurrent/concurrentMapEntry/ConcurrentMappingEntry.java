e9097af4f19e1de4c359f7f6cfc3fe28a2f062fb
zhanlele <leylomai@foxmail.com>
2019-11-29 17:52:06 +0800

    1129-

--- src/myjava/concurrent/concurrentMapEntry/ConcurrentMappingEntry.java
+++ src/myjava/concurrent/concurrentMapEntry/ConcurrentMappingEntry.java
@@ -2,12 +2,20 @@ package myjava.concurrent.concurrentMapEntry;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
 
 public class ConcurrentMappingEntry {
     public static void main(String[] args) {
         Map<String, String> map = new ConcurrentHashMap<>();
         //map.put("1", "2");
-        String oldValue = map.putIfAbsent("1", "1");
+        //String oldValue = map.putIfAbsent("1", "1");
+        String oldValue = map.computeIfAbsent("1", new Function<String, String>() {
+            @Override
+            public String apply(String s) {
+                System.out.println("parameter s = [" + s + "]");
+                return null;
+            }
+        });
 
         System.out.println(oldValue + "###" + map.get("1"));
 
