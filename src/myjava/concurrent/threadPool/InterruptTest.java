a0c2fb87d4359bb9d044c0e46ac2f299268d9328
zhanlele <leylomai@foxmail.com>
2021-03-27 23:35:02 +0800

    0327

---
+++ src/myjava/concurrent/threadPool/InterruptTest.java
@@ -0,0 +1,42 @@
+package myjava.concurrent.threadPool;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class InterruptTest {
+
+    private static final ScheduledExecutorService cancelExec = Executors.newScheduledThreadPool(6);
+
+    public static void timedRun(final Runnable r, long timeout, TimeUnit unit)
+            throws InterruptedException {
+        class RethrowableTask implements Runnable {
+            private volatile Throwable t;
+            @Override
+            public void run() {
+                try {
+                    r.run();
+                } catch (Throwable t) {
+                    this.t = t;
+                }
+            }
+
+            void rethrow() {
+                if (t != null)
+                    throw new RuntimeException(t);
+            }
+        }
+
+        RethrowableTask task = new RethrowableTask();
+        final Thread taskThread = new Thread(task);
+        taskThread.start();
+        cancelExec.schedule(new Runnable() {
+            @Override
+            public void run() {
+                taskThread.interrupt();
+            }
+        }, timeout, unit);
+        taskThread.join(unit.toMillis(timeout));
+        task.rethrow();
+    }
+}
